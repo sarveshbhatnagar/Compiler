cf0a0b967921239e692572c6260042a3df0bc608
Sarvesh Bhatnagar <sarveshbhatnagar08@gmail.com>
2019-07-29 02:44:50 +0530

    Create parser.py

---
+++ Bhasha/src/parser.py
@@ -0,0 +1,16 @@
+
+class Parser(object):
+    """docstring for Parser."""
+
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.token_index = 0
+
+    def parse(self):
+        while self.token_index < len(self.token_index):
+            token_type = self.tokens[self.token_index][0]
+            token_value = self.tokens[self.token_index][1]
+
+            print(token_type, token_value)
+
+            self.token_index += 1
